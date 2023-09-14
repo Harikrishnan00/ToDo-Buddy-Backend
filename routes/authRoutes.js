8ef3aba01fe4abf0cdb004e6d46f8ad252682959
Harikrishnan EV <I0213@TSPL.COM>
2023-09-14 16:31:56 +0530

    logout api added

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,10 @@
 const { Router } = require("express");
-const  {signup,googleLoginSuccess,googleLoginFailure}  = require("../routeHandlers/handleAuthRoutes");
+const {
+  signup,
+  googleLoginSuccess,
+  googleLoginFailure,
+  googleLogout,
+} = require("../routeHandlers/handleAuthRoutes");
 const passport = require("passport");
 
 const router = Router();
@@ -8,21 +13,24 @@ const router = Router();
 router.post("/signup", signup);
 
 // google login success
-router.get('/callback/success', googleLoginSuccess);
+router.get("/callback/success", googleLoginSuccess);
 
 // google login failure
-router.get('/callback/failure', googleLoginFailure)
+router.get("/callback/failure", googleLoginFailure);
 
 router.get(
   "/google",
   passport.authenticate("google", { scope: ["email", "profile"] })
 );
 
-router.get( '/callback',
-    passport.authenticate( 'google', {
-        successRedirect: 'http://localhost:5173',
-        failureRedirect: 'callback/failure'
-}))
+router.get(
+  "/callback",
+  passport.authenticate("google", {
+    successRedirect: "http://localhost:5173/todo",
+    failureRedirect: "callback/failure",
+  })
+);
 
+router.get("/google/logout", googleLogout);
 
 module.exports = router;
